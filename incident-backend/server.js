e6ff77f0aaa03615e9e0d9afa29ac19d0d85a287
mraveendra001 <meghanarahul5430@gmail.com>
2026-05-10 18:14:38 +0530

    changes to cors error

--- incident-backend/server.js
+++ incident-backend/server.js
@@ -30,7 +30,7 @@ async function main() {
   const httpServer = http.createServer(app);
   const io         = new Server(httpServer, {
     cors: {
-      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+      origin: '*',
       methods: ['GET', 'POST'],
     },
   });
